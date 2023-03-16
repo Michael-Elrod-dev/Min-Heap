3e99ec9b539b095f2ea40d060be215231cd9db92
Michael Elrod <117390129+Michael-Elrod-dev@users.noreply.github.com>
2023-03-16 11:18:45 -0400

    Update minHeap.cpp

--- minHeap.cpp
+++ minHeap.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 #include <iostream>
 
-void minHeap::siftUp(int pos){
+// Sifts up the element at the given position
+void minHeap::siftUp(int pos) {
     int parent = (pos - 1) / 2;
     int child = pos;
 
-    while(child != 0 && heap[parent] > heap[child]) {
+    while (child != 0 && heap[parent] > heap[child]) {
         // Swap child and parent
         int temp = heap[parent];
         heap[parent] = heap[child];
@@ -16,78 +17,79 @@ void minHeap::siftUp(int pos){
     }
 }
 
-
-void minHeap::siftDown(int pos){
+// Sifts down the element at the given position
+void minHeap::siftDown(int pos) {
     int parent = pos;
-    // Get left and right of parent
+    // Get left and right children of the parent
     int leftChild = 2 * parent + 1;
     int rightChild = 2 * parent + 2;
     int swap = -1;
     int temp;
 
-    //checking left child
-    if(leftChild < (int)heap.size() && heap[leftChild] < heap[parent])
+    // Check left child
+    if (leftChild < (int)heap.size() && heap[leftChild] < heap[parent])
         swap = leftChild;
 
-    //checking right child
-    if(rightChild < (int)heap.size() && heap[rightChild] < heap[parent])
+    // Check right child
+    if (rightChild < (int)heap.size() && heap[rightChild] < heap[parent]) {
         if (swap == -1)
             swap = rightChild;
-    // Take the minimum
-    else if (heap[rightChild] < heap[leftChild])
-        swap=rightChild;
+        // Take the minimum
+        else if (heap[rightChild] < heap[leftChild])
+            swap = rightChild;
+    }
 
     // No swap needed
-    if(swap == -1)
+    if (swap == -1)
         return;
 
-    // Swap parent with swap value
+    // Swap parent with the swap value
     temp = heap[parent];
     heap[parent] = heap[swap];
     heap[swap] = temp;
     siftDown(swap);
 }
 
-
-minHeap::minHeap(vector<int> data){
-    // Clean heap
+// Constructor that takes a vector of integers as input
+minHeap::minHeap(vector<int> data) {
+    // Clear heap
     heap.resize(0);
 
     // Fill vector
-    for(int & i : data)
+    for (int &i : data)
         heap.push_back(i);
 
-    for(int i=heap.size()-1;i>=0;i--)
+    // Sift down all elements
+    for (int i = heap.size() - 1; i >= 0; i--)
         siftDown(i);
 }
 
-
-void minHeap::insert(int value){
-    // Insert at back of vector
+// Inserts a value into the heap
+void minHeap::insert(int value) {
+    // Insert at the back of the vector
     heap.push_back(value);
-    int position = heap.size()-1;
-    // Move last element up
+    int position = heap.size() - 1;
+    // Move the last element up
     siftUp(position);
 }
 
-
-int minHeap::removeMin(){
+// Removes and returns the minimum value from the heap
+int minHeap::removeMin() {
     int temp;
 
-    // Check if heap is empty
-    if(heap.empty())
+    // Check if the heap is empty
+    if (heap.empty())
         return -1;
 
-    // Swap head with last element
+    // Swap the head with the last element
     temp = heap[0];
-    heap[0] = heap[heap.size()-1];
-    heap[heap.size()-1] = temp;
+    heap[0] = heap[heap.size() - 1];
+    heap[heap.size() - 1] = temp;
     heap.pop_back();
     siftDown(0);
     return temp;
 }
 
-/*
 int main()
 {
     vector<int> input={1,2,3,4,5,11,12,13,14,15};
@@ -111,11 +113,10 @@ int main()
     {
         if (heapTest[i] != test[i])
         {
-            std::cout << "Mismatch in heapTest at position " << i << ": expected/correct value " << test[i] << ", actual value when testing " << heapTest[i] << ".\n";
+            cout << "Mismatch in heapTest at position " << i << ": expected/correct value " << test[i] << ", actual value when testing " << heapTest[i] << ".\n";
             return 1;
         }
     }
-    std::cout << "Passed" << std::endl;
+    cout << "Passed" << endl;
     return 0;
 }
- */
